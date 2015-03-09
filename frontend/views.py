c75086e27daf181ac803b4914b3b74c8cfa174f4
Marc Hibbins <marc.hibbins@guardian.co.uk>
2015-03-09 20:08:28 +0000

    Handle generic errors

--- frontend/views.py
+++ frontend/views.py
@@ -10,3 +10,14 @@ def recipe_list():
     """ Render a list of recipes. """
     recipes = Recipe.query().fetch()
     return render_template('frontend/recipe_list.html', recipes=recipes)
+
+
+def frontend_errors(app):
+    """ Render generic error templates. """
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('errors/404.html'), 404
+
+    @app.errorhandler(500)
+    def server_error_page(error):
+        return render_template('errors/500.html'), 500
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from frontend.views import frontend
+from frontend.views import frontend, frontend_errors
 
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 app = Flask(__name__)
 app.register_blueprint(frontend)
+frontend_errors(app)
