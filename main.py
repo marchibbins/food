148eae213b7f4c05f74aab264a34d1a570a1fc0d
Marc Hibbins <marc.hibbins@guardian.co.uk>
2015-03-10 18:07:22 +0000

    Add fake key

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ from frontend.views import frontend, frontend_errors
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 app = Flask(__name__)
+app.secret_key = 'SECRET_KEY_LOL'
 app.register_blueprint(frontend)
 frontend_errors(app)
