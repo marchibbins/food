2282e3778b7d8a5d076e5e642a5e92bfddec68c8
Marc Hibbins <marchibbins@gmail.com>
2015-03-17 17:10:42 +0000

    Hack method for JSON parsing data

--- food/parser.py
+++ food/parser.py
@@ -1,41 +1,81 @@
 from google.appengine.ext import ndb
+import json
 
 from food.models import Ingredient, Measures, Quantity, Recipe
 
 
 def resetdb():
     """ Quick delete and reload of database. """
-    if delete() and put():
-        return "Data reset"
-    else:
-        return "Something went wrong"
+    try:
+        delete()
+        load_json()
+        return 'Data reset'
+    except Exception as error:
+        return '%s' % error
 
 
 def delete():
     """ Deletes all Ingredients and Recipes. """
-    return ndb.delete_multi(
+    ndb.delete_multi(
         Ingredient.query().fetch(keys_only=True) +
         Recipe.query().fetch(keys_only=True)
     )
 
 
-def put():
-    """ Puts all dummy Ingredients and Recipes. """
-    one = Ingredient(name='One', slug='one', measure=Measures.GRAMS)
-    two = Ingredient(name='Two', slug='two', measure=Measures.MILLILITRES)
-    three = Ingredient(name='Three', slug='three', measure=Measures.UNITS)
-    ndb.put_multi([
-        one,
-        two,
-        three,
-    ])
-    return ndb.put_multi([
-        Recipe(name='A', slug='a', method="Make.", quantities=[
-            Quantity(ingredient=one.key, amount=10),
-            Quantity(ingredient=two.key, amount=12),
-            Quantity(ingredient=three.key, amount=5),
-        ]),
-        Recipe(name='B', slug='b', method="Make.", quantities=[
-            Quantity(ingredient=one.key, amount=3),
-        ]),
-    ])
+def load_json():
+    """ Attempt to parse and put from JSON. """
+    # Ingredients
+    ingredients_file = open('json/ingredients.json')
+    ingredients_json = json.load(ingredients_file)
+
+    slugs = map(lambda i: i['slug'], ingredients_json)
+    if len(slugs) != len(set(slugs)):
+        raise Exception('Duplicate ingredients found in JSON')
+
+    ingredients = {}
+    for ingredient in ingredients_json:
+        for prop in ['name', 'slug', 'measure']:
+            if not ingredient.get(prop):
+                raise Exception('Ingredient missing %s' % prop)
+        slug = ingredient['slug']
+        if ingredient['measure'] not in Measures.choices:
+            raise Exception('"%s" not in measures' % ingredient['measure'])
+        if Ingredient.query(Ingredient.slug == slug).get():
+            raise Exception('Ingredient "%s" already exists' % slug)
+        ingredients[slug] = Ingredient(name=ingredient['name'],
+                                       slug=slug,
+                                       measure=ingredient['measure'])
+    ingredients_file.close()
+    ndb.put_multi(ingredients.values())
+
+    # Recipes
+    recipes_file = open('json/recipes.json')
+    recipes_json = json.load(recipes_file)
+
+    slugs = map(lambda i: i['slug'], recipes_json)
+    if len(slugs) != len(set(slugs)):
+        raise Exception('Duplicate recipes found in JSON')
+
+    recipes = []
+    for recipe in recipes_json:
+        for prop in ['name', 'slug']:
+            if not recipe.get(prop):
+                raise Exception('Recipe missing %s' % prop)
+        if Recipe.query(Recipe.slug == recipe['slug']).get():
+            raise Exception('Recipe "%s" already exists' % recipe['slug'])
+        quantities = []
+        for quantity in recipe['quantities']:
+            for prop in ['slug', 'amount']:
+                if not quantity.get(prop):
+                    raise Exception('Quantity missing %s' % prop)
+            slug = quantity['slug']
+            if not ingredients.get(slug):
+                raise Exception('Ingredient "%s" not found' % slug)
+            quantities.append(Quantity(ingredient=ingredients[slug].key,
+                                       amount=quantity['amount']))
+        recipes.append(Recipe(name=recipe['name'],
+                              slug=recipe['slug'],
+                              method=recipe['method'],
+                              quantities=quantities))
+    recipes_file.close()
+    ndb.put_multi(recipes)
